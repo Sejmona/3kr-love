a9946979eb9d21173d13fcbd3f7e2e71b0fe384e
Simona <simona.smelhausova@gmail.com>
2022-06-04 16:14:39 +0200

    druhyCommit

--- my-app/src/index.jsx
+++ my-app/src/index.jsx
@@ -1,22 +1,16 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Mapa } from './komponenty/Mapa';
 import './style.css';
 
 const App = () => (
   <div className="container">
     <header>
       <div className="logo"></div>
-      <h1>Webová aplikace</h1>
     </header>
-    <main>
-      <p>Startovací šablona pro webovou aplikaci v Reactu. Vytvořeno pomocí <a href="https://www.npmjs.com/package/create-czechitas-app">create-czechitas-app</a>.</p>
-    </main>
-    <footer>
-      <p>Czechitas, Digitální akademie: Web</p>
-    </footer>
+    <Mapa />
+    <footer></footer>
   </div>
 );
 
-createRoot(
-  document.querySelector('#app')
-).render(<App />);
+createRoot(document.querySelector('#app')).render(<App />);
