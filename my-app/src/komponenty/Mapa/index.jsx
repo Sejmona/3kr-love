61dcc0b232777765ad64620df16d8eff22e70774
Simona <simona.smelhausova@gmail.com>
2022-06-04 10:09:02 +0200

    SecondCommit

---
+++ my-app/src/komponenty/Mapa/index.jsx
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from 'react';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import ReactMapGL, {
+  Marker,
+  Popup,
+  NavigationControl,
+  GeolocateControl,
+} from 'react-map-gl';
