244faef8143a7972e902df8fc051d8fc3761e262
Simona <simona.smelhausova@gmail.com>
2022-06-05 19:12:22 +0200

    pridane foto

--- my-app/src/komponenty/Pribeh/pribeh.jsx
+++ my-app/src/komponenty/Pribeh/pribeh.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './pribeh.css';
+import balaban from './../../img/balaban.jpg';
+import masin from './../../img/masin.jpg';
+import moravek from './../../img/moravek.jpg';
 
 export const Pribeh = () => {
   return (
@@ -10,13 +13,13 @@ export const Pribeh = () => {
         <img src="masin.jpg" alt="masin" />
         <img src="moravek.jpg" alt="moravek" />
         <div className="clanek">
-          <p1>
+          <p>
             Tři králové - členové českého protinacistického odboje Tento příběh
             se odehrává v letech 1939 – 1942 za okupace. Kdy stovky odvážných
             Čechoslováků tajně bojovali proti nenáviděnému fašizmu, který měl v
             plánu zlikvidovat mnoho ostatních národů. Pojďte s námi po stopách
             těch nejodvážnějších:
-          </p1>
+          </p>
         </div>
       </section>
     </body>
