50b63275a66e301ed14d1dbc39d58bb6f9e08027
Klára Václavíková <klarka.vaclvikovaa@gmail.com>
2022-06-05 12:40:05 +0200

    Routes Refactor

--- my-app/src/index.jsx
+++ my-app/src/index.jsx
@@ -1,33 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-<<<<<<< Updated upstream
-import { Mapa } from '../public/Komponenty/Mapa';
-=======
->>>>>>> Stashed changes
 import './style.css';
-<<<<<<< HEAD
 import { Homepage } from './pages/Homepage/homepage';
 import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom';
 
 const App = () => (
   <div className="container">
     <Homepage />
-=======
-import Stepper from '../public/Komponenty/Stepper';
-import Vizitka from '../public/Komponenty/Vizitka';
-import source from '../public/data';
-import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom';
-
-const App = () => (
-  <div className="container">
-    <header>
-      <div className="logo"></div>
-    </header>
-    <Stepper />
-    <Mapa />
-    <Vizitka stepData={source[0]} />
-    <footer></footer>
->>>>>>> Stepper
   </div>
 );
 
